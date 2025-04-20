e65d953d326d164ab1f15d8422e517ee0ebf658e
fer-r <fer_r_a@outlook.es>
2025-04-21 01:51:32 +0200

    Docker backend works

--- docker/database/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE secretosFRA (
- id INT AUTO_INCREMENT PRIMARY KEY,
- fraseFRA VARCHAR(255) NOT NULL
-);
-INSERT INTO secretosFRA (fraseFRA) VALUES ('La base de datos responde, buenos días Fernando Rodriguez Arcosº')

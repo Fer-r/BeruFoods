c9dfac584a10c6a8c3af6952cf1b0ad0743cdcd4
fer-r <fer_r_a@outlook.es>
2025-04-27 02:56:28 +0200

    Update Docker and Makefile configurations for improved functionality
    This commit modifies the Docker setup by changing the database image from MySQL to MariaDB, updating health check commands, and refining the Dockerfile for the frontend. Additionally, the Makefile is enhanced to include log tailing during project initialization and adjusts user permissions for backend commands. The .gitignore is also updated to exclude specific environment files.

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 set -e
+
+CERT_COUNTRY=${CERT_COUNTRY:-"ES"}
+CERT_STATE=${CERT_STATE:-"Granada"}
+CERT_CITY=${CERT_CITY:-"Granada"}
+CERT_ORG=${CERT_ORG:-"IESHLANZ"}
+CERT_OU=${CERT_OU:-"DAWT"}
+CERT_CN=${CERT_CN:-"Fernando Rodriguez Arcos"}
+CERT_EMAIL=${CERT_EMAIL:-"frodarc876@g.educaand.es"}
+
 # Verifica si el certificado ya existe
 if [ ! -f /etc/nginx/certs/selfsigned.crt ]; then
  echo "Generando certificado autofirmado..."
  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
- -subj "/C=ES/ST=Granada/L=Granada/O=IESHLANZ/OU=DAWT/CN=Fernando_Rodriguez_Arcos/emailAddress=FRA@ieshlanz.es" \
+ -subj "/C=${CERT_COUNTRY}/ST=${CERT_STATE}/L=${CERT_CITY}/O=${CERT_ORG}/OU=${CERT_OU}/CN=${CERT_CN}/emailAddress=${CERT_EMAIL}" \
  -keyout /etc/nginx/certs/selfsigned.key \
  -out /etc/nginx/certs/selfsigned.crt
 fi

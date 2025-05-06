edd46025a4841b0200676c51f692972ec193d23c
fer-r <fer_r_a@outlook.es>
2025-05-07 00:24:14 +0200

    Improve restaurant filtering, add custom DQL functions, and enhance configurations

---
+++ backend/src/Doctrine/DQL/Sqrt.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Doctrine\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+/**
+ * "SQRT" "(" SimpleArithmeticExpression ")"
+ */
+class Sqrt extends FunctionNode
+{
+    public $simpleArithmeticExpression;
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'SQRT(' . $this->simpleArithmeticExpression->dispatch($sqlWalker) . ')';
+    }
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $this->simpleArithmeticExpression = $parser->SimpleArithmeticExpression();
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+} 
--- backend/src/Entity/RestaurantAddress.php
+++ backend/src/Entity/RestaurantAddress.php
@@ -4,39 +4,19 @@ namespace App\Entity;
 
 use App\Repository\RestaurantAddressRepository;
 use Doctrine\ORM\Mapping as ORM;
+// Assert import might be needed if RestaurantAddress adds specific constraints
+// For now, assuming common constraints are in Address base class
 
 #[ORM\Entity(repositoryClass: RestaurantAddressRepository::class)]
 #[ORM\Table(name: 'restaurant_addresses')]
-class RestaurantAddress
+class RestaurantAddress extends Address 
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    // Id is inherited from Address.
 
     // OneToOne because SQL has UNIQUE constraint on restaurant_id
-    #[ORM\OneToOne(targetEntity: Restaurant::class, inversedBy: 'address')] // Assuming Restaurant will have an 'address' property
-    #[ORM\JoinColumn(name: 'restaurant_id', referencedColumnName: 'id', nullable: false)] // Match foreign key
+    #[ORM\OneToOne(targetEntity: Restaurant::class, inversedBy: 'address')]
+    #[ORM\JoinColumn(name: 'restaurant_id', referencedColumnName: 'id', nullable: false)]
     private ?Restaurant $restaurant = null;
-
-    #[ORM\Column(type: 'text', nullable: false)]
-    private ?string $address_line = null;
-
-    #[ORM\Column(type: 'decimal', precision: 10, scale: 7, nullable: false)]
-    private ?string $lat = null;
-
-    #[ORM\Column(type: 'decimal', precision: 10, scale: 7, nullable: false)]
-    private ?string $lng = null;
-
-    #[ORM\Column(type: 'text', nullable: false)]
-    private ?string $province = null;
-
-    // --- Getters and Setters ---
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getRestaurant(): ?Restaurant
     {
@@ -49,47 +29,4 @@ class RestaurantAddress
         return $this;
     }
 
-    public function getAddressLine(): ?string
-    {
-        return $this->address_line;
-    }
-
-    public function setAddressLine(?string $address_line): static
-    {
-        $this->address_line = $address_line;
-        return $this;
-    }
-
-    public function getLat(): ?string
-    {
-        return $this->lat;
-    }
-
-    public function setLat(?string $lat): static
-    {
-        $this->lat = $lat;
-        return $this;
-    }
-
-    public function getLng(): ?string
-    {
-        return $this->lng;
-    }
-
-    public function setLng(?string $lng): static
-    {
-        $this->lng = $lng;
-        return $this;
-    }
-
-    public function getProvince(): ?string
-    {
-        return $this->province;
-    }
-
-    public function setProvince(?string $province): static
-    {
-        $this->province = $province;
-        return $this;
-    }
 } 

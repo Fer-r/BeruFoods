7a267714d904f23d9c44d6d82dca055032523739
Fer-r <77117601+Fer-r@users.noreply.github.com>
2025-05-31 23:12:26 +0200

    Add Mercure dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import { router } from "./router";
-import { RouterProvider } from "react-router"; // Correct import
-import { useThemeManager } from "./hooks/useThemeManager"; // Import the hook
-import { AuthProvider } from "./context/AuthContext.jsx"; // Import AuthProvider
-import { ModalProvider } from "./context/ModalContext.jsx"; // Import ModalProvider
+import { RouterProvider } from "react-router";
+import { useThemeManager } from "./hooks/useThemeManager";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ModalProvider } from "./context/ModalContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
+import { NotificationProvider } from "./context/NotificationContext.jsx";
 
 const App = () => {
-  useThemeManager(); // Call the hook to activate global theme management
+  useThemeManager();
 
   return (
     <AuthProvider>
       <ModalProvider>
         <CartProvider>
-        <RouterProvider router={router} />
+          <NotificationProvider>
+            <RouterProvider router={router} />
+          </NotificationProvider>
         </CartProvider>
       </ModalProvider>
     </AuthProvider>
